7562e70871627d3e04dfebf3a3ae909a424294da
IoanaPedroo <ioanaserban.iulia@gmail.com>
2021-12-08 18:25:11 +0200

    upload img

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, url_for, redirect
 import data_manager
 import data_handler
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-
+app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), "static", "image")
 
 
 @app.route("/")
@@ -13,14 +15,14 @@ def hello():
 
 @app.route('/question/<question_id>/new-answer', methods=['GET', 'POST'])
 def new_answer(question_id):
-    return render_template('new_answer.html', new_answer=new_answer)
+    return render_template('new_answer.html', new_answer=new_answer, question_id=question_id)
 
 
 @app.route('/question/<question_id>', methods=['GET', 'POST'])
 def display_question(question_id):
     if request.method=="POST":
         if 'message' in request.form:
-            new_answer=request.form['message']
+            new_answer = request.form['message']
             data_manager.write_answer(new_answer, question_id)
         if 'question_message' in request.form:
             new_question=request.form['question_message']
@@ -28,8 +30,8 @@ def display_question(question_id):
         answers=data_handler.get_questions('sample_data/answer.csv')
        
     for answer in answers:
-        if answer['id']==answer_id:
-            answer['vote_number'] = int( answer['vote_number'])+1
+        if answer['id'] == answer_id:
+            answer['vote_number'] = int(answer['vote_number'])+1
             data_handler.save_data_to_csv(answers, 'sample_data/answer.csv', data_handler.DATA_HEADER1)
     return render_template('question.html',question=data_manager.get_question(question_id), answers=data_manager.get_dict('sample_data/answer.csv') )
 
@@ -41,11 +43,11 @@ def delete(question_id):
 
 @app.route("/answer/<answer_id>/delete", methods=["GET", "POST"])
 def delete_answer(answer_id):
-    answers = data_handler.get_all_user_story("/home/ioana/ask-mate-1-python-IoanaPedroo/sample_data/answer.csv")
+    answers = data_handler.get_all_user_story("sample_data/answer.csv")
     for answer in answers:
         if answer['id'] == answer_id:
             answers.remove(answer)
-            data_handler.save_data_to_csv(answers, "/home/ioana/ask-mate-1-python-IoanaPedroo/sample_data/answer.csv",  data_handler.DATA_HEADER1)
+            data_handler.save_data_to_csv(answers, "sample_data/answer.csv",  data_handler.DATA_HEADER1)
     return redirect("/question/<question_id>")
 
 # @app.route('/question/<answer_id>/vote_up', methods=["GET", "POST"])
@@ -78,7 +80,7 @@ def edit_question(question_id):
 
 @app.route("/list")
 def list_q():
-    questions = data_handler.get_questions("/home/ioana/ask-mate-1-python-IoanaPedroo/sample_data/question.csv")
+    questions = data_handler.get_questions("sample_data/question.csv")
     header = data_handler.DATA_HEADER
     return render_template('list.html', questions=questions, header=header)
 
@@ -87,7 +89,7 @@ def add_q(id=None):
     if request.method == 'GET':
         return render_template('add-question.html', id=id)
     elif request.method == 'POST':
-        questions = data_handler.get_questions("/home/ioana/ask-mate-1-python-IoanaPedroo/sample_data/question.csv")
+        questions = data_handler.get_questions("sample_data/question.csv")
         result = {}
         result['id'] = len(questions)+1
         result['submission_time'] = request.form.get('submission_time')
@@ -95,24 +97,15 @@ def add_q(id=None):
         result['vote_number'] = request.form.get('vote_number')
         result['title'] = request.form.get('title')
         result['message'] = request.form.get('message')
-        #result['image'] = request.files('image')
+        if request.files:
+            image = request.files['image']
+            if image.filename != '':
+                path = secure_filename(os.path.join(app.config['UPLOAD_FOLDER'], image.filename))
+                image.save(path)
+            result['image'] = image
         questions.append(result)
-        data_handler.save_data_to_csv(questions, "/home/ioana/ask-mate-1-python-IoanaPedroo/sample_data/question.csv",  data_handler.DATA_HEADER)
+        data_handler.save_data_to_csv(questions, "sample_data/question.csv", data_handler.DATA_HEADER)
         return redirect("/question/<question_id>")
-
-
-
-# def upload_image():
-#     if 'image' not in request.files:
-#         flash('No file apart')
-#         return redirect('/add-question')
-#     image = request.files['image']
-#     if file.filename == '':
-#         flash('No image selected for uploding')
-#         return redirect('/add-question')
-#     if file and allowe_file(file.filename):
-#         filename = secure_filename(file.filename)
-#     pass
 
 
 if __name__ == "__main__":
